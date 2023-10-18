018619251b53afd0a800eeb8445f222e12dc8eeb
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-10-17 21:59:57 -0400

    init

---
+++ src/main/java/com/caloriestracker/app/model/UserMeal.java
@@ -0,0 +1,34 @@
+package com.caloriestracker.app.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * Danylo Sashchuk
+ * 10/17/23
+ */
+
+public class UserMeal {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    public UserMeal(LocalDateTime dateTime, String description, int calories) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+}
---
+++ src/main/java/com/caloriestracker/app/util/TimeUtil.java
@@ -0,0 +1,14 @@
+package com.caloriestracker.app.util;
+
+import java.time.LocalTime;
+
+/**
+ * Danylo Sashchuk
+ * 10/17/23
+ */
+
+public class TimeUtil {
+    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+        return !lt.isBefore(startTime) && lt.isBefore(endTime);
+    }
+}
