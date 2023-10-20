bea07c1545f1dbd9cf847f6d0536749eef4ee5bc
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-10-19 22:24:07 -0400

    maven init

--- src/main/java/com/caloriestracker/app/util/UserMealsUtil.java
+++ src/main/java/com/caloriestracker/app/util/UserMealsUtil.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> meals = Arrays.asList(new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак"
-                , 500), new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                        , 500), new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
